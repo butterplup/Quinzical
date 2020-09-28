a401c06e9707de0b3fef4b3fc2f8c97ddb8bc385
Joel K Hutchinson <joelhutchinson@MacBook-Pro.fritz.box>
2020-09-29 00:29:59 +1300

    Moved SpeakerThread to correct package, fixed bug in MainMenu where exitBtn was improperly defined

---
+++ Quinzical/src/gamelogic/SpeakerThread.java
@@ -0,0 +1,27 @@
+package gamelogic;
+
+
+public class SpeakerThread extends Thread {
+
+	private String _message;
+	
+	public SpeakerThread(String message) {
+		_message = message;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			// Command reads out each the desired message
+			String command = "espeak \"" + _message + "\"";
+			ProcessBuilder pb = new ProcessBuilder("bash", "-c", command);
+			// Executes command
+			pb.start();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
