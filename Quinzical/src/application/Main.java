9ee4880de3800a4babb97be7b263e8481b2c9d2b
Joel K Hutchinson <joelhutchinson@MacBook-Pro.fritz.box>
2020-09-29 01:05:49 +1300

    Cleaned up imports

--- Quinzical/src/application/Main.java
+++ Quinzical/src/application/Main.java
@@ -1,15 +1,9 @@
 package application;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Main extends Application{
