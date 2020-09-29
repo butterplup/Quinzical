fdbf0ff2c5685b3cdc58b07e8e2e0fc5eb4ac65e
se2062020 <se2062020@se2062020-VirtualBox>
2020-09-29 15:41:55 +1300

    GameBoard logic done
    Should now be able to play, implement gui interaction.
    NO load/savestate still.

--- Quinzical/src/gamelogic/Question.java
+++ Quinzical/src/gamelogic/Question.java
@@ -1,31 +1,27 @@
 package gamelogic;
 
-public class Question {
-	private int _reward;
+import java.io.Serializable;
+
+public class Question implements Serializable{
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	//	private int _reward;
 	private String _question;
 	private String _answer;
-	private boolean _completed;
-	public Question(int reward, String question, String answer) {
-		_reward = reward;
+	private String _whatX;
+	public Question(String question, String answer, String whatX) {
+//		_reward = reward;
 		_question = question;
 		_answer = answer;
-		_completed = false;
-
-	}
-	
-	public boolean isCompleted() {
-		return _completed;
-	}
-	
-	public int getReward() {
-		return _reward;
+		_whatX = whatX;
 	}
 	
 	public void ask() {
-		_completed=true;
+		say(_question);
 		//TTS the question
 		//tts.say(_question);
-	
 	}
 	
 	public boolean answer(String answer) {
@@ -33,18 +29,17 @@ public class Question {
 		if (answer.equalsIgnoreCase(_answer))//Answer was correct
 		{
 			//winnings.add
-			//tts.say();
+			say("Your answer was correct!");
 			return true;
 		}
 		else {//Answer was wrong			
 		
-				//tts.say();
+			say("Wrong, the correct answer was " + _answer);
 			return false;
 		}
 	}
-	
-	public void reset() {
-		_completed = false;
-	}
 
+	private void say(String message) {
+		new SpeakerThread(_question);
+	}
 }
