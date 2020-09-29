1845de670441bf7090384fc8c5377fd7ef1972b2
Joel K Hutchinson <joelhutchinson@wifi-172-23-41-249.uoa-wifi.auckland.ac.nz>
2020-09-29 18:53:37 +1300

    Can now move between the menus for practice module and main menu

--- Quinzical/src/application/view/MainMenuController.java
+++ Quinzical/src/application/view/MainMenuController.java
@@ -1,8 +1,10 @@
 package application.view;
 
+import application.Main;
 import javafx.fxml.*;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
@@ -14,6 +16,9 @@ public class MainMenuController {
     private Button practiceBtn;
     @FXML
     private Button exitBtn;
+    
+    @FXML
+    private AnchorPane anchor;
 
     @FXML
     public void handleGamesBtnClick() {
@@ -23,6 +28,19 @@ public class MainMenuController {
     @FXML
     public void handlePracticeBtnClick() {
         System.out.println("Prac Button clicked");
+        Stage mainStage = (Stage) practiceBtn.getScene().getWindow();
+        try {
+
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("view/PracticeMenu.fxml"));
+            AnchorPane rootLayout = (AnchorPane) loader.load();
+            anchor.getChildren().setAll(rootLayout);
+
+            mainStage.setHeight(400.0);
+            mainStage.setWidth(600.0);
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        } 
     }
     
     @FXML
