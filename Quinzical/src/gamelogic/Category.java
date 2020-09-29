849dfb2627ed097964dc85e68596b6211c4027ac
B Song <billsong19@hotmail.com>
2020-09-29 15:41:55 +1300

    GameBoard logic done
    Should now be able to play, implement gui interaction.
    NO load/savestate still.

--- Quinzical/src/gamelogic/Category.java
+++ Quinzical/src/gamelogic/Category.java
@@ -1,10 +1,49 @@
 package gamelogic;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
-public class Category {
+public class Category implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String _name;
 	private List<Question> _questions = new ArrayList<Question>();
+	
+	public Category(File file) {
+		try {
+			_name = file.getName();
+			
+            Scanner myReader = new Scanner(file);
+            while (myReader.hasNextLine())
+            {
+                String str = myReader.nextLine();
+                String questionLine[] = str.split("\\|");
+
+                _questions.add(new Question(questionLine[0], questionLine[2], questionLine[1]));
+                
+            }
+            myReader.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+	}
+	
+//	public int[] getRandom() {
+//		int [] output = {0,0,0,0,0};
+//		return output;
+//	}
+	
+	public void shuffle() {
+		Collections.shuffle(_questions);
+	}
 	
 	public void ask(int qstnindex)
 	{
@@ -15,5 +54,9 @@ public class Category {
 	{
 		boolean correct = _questions.get(qstnindex).answer(answer);
 		return correct;
+	}
+	
+	public String getName() {
+		return _name;
 	}
 }
