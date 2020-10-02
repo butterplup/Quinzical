f9fbc244dfa2ffe18a3a8093c37cd4c5f947c8f3
Joel K Hutchinson <joelhutchinson@MacBook-Pro.fritz.box>
2020-10-02 17:07:23 +1300

    Created fxml and controller files for Game Module GUI

---
+++ Quinzical/src/application/view/GameMenuController.java
@@ -0,0 +1,61 @@
+package application.view;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+
+public class GameMenuController implements Initializable {
+
+    @FXML
+    private ChoiceBox<String> selectCategory;
+    @FXML
+    private Button backBtn;
+    @FXML
+    private AnchorPane anchor;
+
+    public void handleCategorySelected() {
+        System.out.println("Category selected: " + selectCategory.getValue());
+        // Make category selector BorderPane opacity = 0
+        // Make question presenter BorderPane opacity = 1
+    }
+    
+    public void handleBackBtnClick() {
+    	System.out.println("Back");
+    	
+    	// Get the primaryStage object
+    	Stage mainStage = (Stage) backBtn.getScene().getWindow();
+        try {
+        	// Load the main menu layout
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("view/MainMenu.fxml"));
+            AnchorPane rootLayout = (AnchorPane) loader.load();
+            
+            // Change layout from practice menu to main menu
+            anchor.getChildren().setAll(rootLayout);
+            
+            // Adjust bounds of stage
+            mainStage.setHeight(400.0);
+            mainStage.setWidth(600.0);
+
+        } catch(Exception e) {
+        	// Print in case of any errors
+            e.printStackTrace();
+        } 
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    	
+    	// Make some call to the gameboard object to get all the actual categories
+        selectCategory.getItems().addAll("Animals", "Countries", "Food");
+    	
+    }
+    
+}
